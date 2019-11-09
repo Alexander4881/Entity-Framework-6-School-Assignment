b7717b5711c9680aee5844f370ddd45a40c43972
Alexander4881 <alihnmikkelsen@gmail.com>
2019-11-09 13:29:49 +0100

    Add project files.

---
+++ EntityFramework6/Classes/BaseEntity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework6.Classes
+{
+    [Table("BaseEntity")]
+    class BaseEntity
+    {
+        [Key]
+        public string Name { get; internal set; }
+        public int Health { get; internal set; }
+        public int Attack { get; internal set; }
+
+        // constructor
+        public BaseEntity() { }
+        public BaseEntity(string name, int health, int attack)
+        {
+            Name = name;
+            Health = health;
+            Attack = attack;
+        }
+
+        #region Methods
+        
+        /// <summary>
+        /// Method from subtracting health
+        /// </summary>
+        /// <param name="damage"></param>
+        public void TakeDamage(int damage)
+        {
+            if ((Health - damage) >= 0)
+            {
+                // subtract damage from health
+                Health = 0;
+            }
+            else if (Health <= 0)
+            {
+                Health = Health - damage;
+            }
+        }
+        #endregion
+    }
+}
---
+++ EntityFramework6/DatabaseAccessLayer/EntityFrameworkContext.cs
@@ -0,0 +1,20 @@
+﻿using EntityFramework6.Classes;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework6.DatabaseAccessLayer
+{
+    class EntityFrameworkContext:DbContext
+    {
+        public EntityFrameworkContext():base("EntityFramework6")
+        {
+            Database.SetInitializer<EntityFrameworkContext>(new DropCreateDatabaseIfModelChanges<EntityFrameworkContext>());
+        }
+
+        public DbSet<BaseEntity> Characters { get; set; }
+    }
+}
